b924b9bf11302a7219a80d09fc61d443344fb7b5
dimyasha7123 <d_galushkin@mail.ru>
2022-05-22 02:43:19 +0500

    add quizserver handler

--- internal/app/update_handler.go
+++ internal/app/update_handler.go
@@ -3,6 +3,7 @@ package app
 import (
 	"context"
 	"fmt"
+	pb "gitlab.ozon.dev/dimayasha7123/homework-2-dimayasha-7123/pkg/api"
 	"hw2-tgbot/internal/models"
 	"log"
 	"strings"
@@ -29,22 +30,33 @@ func (b *bclient) updateHandler(ctx context.Context, update models.Update) error
 		var fmtText string
 
 		if ok {
+
 			fmtText = "Welcome, %s. Again"
-			// тут можно апдейтить инфу о юзере, если понадобится
-			// например, у юзера может поменяться username.
+			// TODO update user's username
+
 		} else {
+
 			fmtText = "Welcome, %s"
 			user = models.NewUser(
 				update.Message.From.ID,
 				update.Message.From.Username,
 			)
+
+			qsID, err := b.quizClient.AddUser(ctx, &pb.User{Name: fmt.Sprintf("%d", user.TGID)})
+			if err != nil {
+				return err
+			}
+			user.QSID = qsID.ID
+
+			err = b.repo.AddUser(ctx, user)
+			if err != nil {
+				return err
+			}
+
 			b.users.Lock()
 			b.users.M[user.TGID] = user
 			b.users.Unlock()
-			err := b.repo.AddUser(ctx, user)
-			if err != nil {
-				return nil
-			}
+
 		}
 
 		text = fmt.Sprintf(fmtText, user.Username)
